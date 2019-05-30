f2ed2b69ea29a5588e5a8b96326ae8ea82dafe50
houjie <houjie@qutoutiao.net>
2019-05-30 22:15:59 +0800

    完善启动adsl服务脚本

--- adsl_server/auto_bash/adsl_run_server.sh
+++ adsl_server/auto_bash/adsl_run_server.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 PATH=/root/venv/python3-forcrawl/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
+curday=`date +%Y-%m-%d-%H%M%S`
 server_process_name="adsl_server.py"
 cd /root/code/spiders/adsl_server
 ps -fe|grep $server_process_name |grep -v grep
 if [ $? -ne 0 ]
 then
-    python $server_process_name >> /data/logs/server/adsl_server.log 2>&1 &
+    python $server_process_name >> /dev/null 2>&1 &
     echo "$server_process_name start process.....$curday"
 else
     echo "runing....."
