5f0dff2895d739408d78498c49f0136ccf10ea48
houjie <houjie@qutoutiao.net>
2019-09-19 17:42:39 +0800

    中国土地网验证码图片解码-data:image/png;base64图片编码解码

--- base_method.py
+++ base_method.py
@@ -1,9 +1,72 @@
 import random
 import base64
+import uuid
+
 from base_settings import *
 from twilio.rest import Client
 import yagmail
 import hashlib
+
+def decode_image(src):
+    """
+    解码图片
+    :param src: 图片编码
+        eg:
+            src="data:image/gif;base64,R0lGODlhMwAxAIAAAAAAAP///
+                yH5BAAAAAAALAAAAAAzADEAAAK8jI+pBr0PowytzotTtbm/DTqQ6C3hGX
+                ElcraA9jIr66ozVpM3nseUvYP1UEHF0FUUHkNJxhLZfEJNvol06tzwrgd
+                LbXsFZYmSMPnHLB+zNJFbq15+SOf50+6rG7lKOjwV1ibGdhHYRVYVJ9Wn
+                k2HWtLdIWMSH9lfyODZoZTb4xdnpxQSEF9oyOWIqp6gaI9pI1Qo7BijbF
+                ZkoaAtEeiiLeKn72xM7vMZofJy8zJys2UxsCT3kO229LH1tXAAAOw=="
+
+    :return: str 保存到本地的文件名
+    """
+    # 1、信息提取
+    result = re.search("data:image/(?P<ext>.*?);base64,(?P<data>.*)", src, re.DOTALL)
+    if result:
+        ext = result.groupdict().get("ext")
+        data = result.groupdict().get("data")
+
+    else:
+        raise Exception("Do not parse!")
+
+    # 2、base64解码
+    img = base64.urlsafe_b64decode(data)
+    print(img)
+    # 3、二进制文件保存
+    filename = "images/{}.{}".format(uuid.uuid4(), ext)
+    with open(filename, "wb") as f:
+        f.write(img)
+
+    return filename
+
+
+def encode_image(filename):
+    """
+    编码图片
+    :param filename: str 本地图片文件名
+    :return: str 编码后的字符串
+        eg:
+        src="data:image/gif;base64,R0lGODlhMwAxAIAAAAAAAP///
+            yH5BAAAAAAALAAAAAAzADEAAAK8jI+pBr0PowytzotTtbm/DTqQ6C3hGX
+            ElcraA9jIr66ozVpM3nseUvYP1UEHF0FUUHkNJxhLZfEJNvol06tzwrgd
+            LbXsFZYmSMPnHLB+zNJFbq15+SOf50+6rG7lKOjwV1ibGdhHYRVYVJ9Wn
+            k2HWtLdIWMSH9lfyODZoZTb4xdnpxQSEF9oyOWIqp6gaI9pI1Qo7BijbF
+            ZkoaAtEeiiLeKn72xM7vMZofJy8zJys2UxsCT3kO229LH1tXAAAOw=="
+
+    """
+    # 1、文件读取
+    ext = filename.split(".")[-1]
+
+    with open(filename, "rb") as f:
+        img = f.read()
+
+    # 2、base64编码
+    data = base64.b64encode(img).decode()
+
+    # 3、图片编码字符串拼接
+    src = "data:image/{ext};base64,{data}".format(ext=ext, data=data)
+    return src
 
 def sub_str(a, b):
     c = a[:int(b[0])]
--- spiders/china_land.py
+++ spiders/china_land.py
@@ -2,39 +2,6 @@ import uuid
 
 from decrypt_methed import *
 
-def decode_image(src):
-    """
-    解码图片
-    :param src: 图片编码
-        eg:
-            src="data:image/gif;base64,R0lGODlhMwAxAIAAAAAAAP///
-                yH5BAAAAAAALAAAAAAzADEAAAK8jI+pBr0PowytzotTtbm/DTqQ6C3hGX
-                ElcraA9jIr66ozVpM3nseUvYP1UEHF0FUUHkNJxhLZfEJNvol06tzwrgd
-                LbXsFZYmSMPnHLB+zNJFbq15+SOf50+6rG7lKOjwV1ibGdhHYRVYVJ9Wn
-                k2HWtLdIWMSH9lfyODZoZTb4xdnpxQSEF9oyOWIqp6gaI9pI1Qo7BijbF
-                ZkoaAtEeiiLeKn72xM7vMZofJy8zJys2UxsCT3kO229LH1tXAAAOw=="
-
-    :return: str 保存到本地的文件名
-    """
-    # 1、信息提取
-    result = re.search("data:image/(?P<ext>.*?);base64,(?P<data>.*)", src, re.DOTALL)
-    if result:
-        ext = result.groupdict().get("ext")
-        data = result.groupdict().get("data")
-
-    else:
-        raise Exception("Do not parse!")
-
-    # 2、base64解码
-    img = base64.urlsafe_b64decode(data)
-    print(img)
-    # 3、二进制文件保存
-    filename = "images/{}.{}".format(uuid.uuid4(), ext)
-    with open(filename, "wb") as f:
-        f.write(img)
-
-    return filename
-
 url = "http://www.landchina.com/default.aspx?tabid=226"
 session = requests.session()
 res = session.get(url)
